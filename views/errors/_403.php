cff82e9ec7a56f8bb9169e73f711e2856152383f
ShiroAky <martinezfranklinjunior7@gmail.com>
2023-08-13 14:17:53 -0400

    V2.5.1

---
+++ views/errors/_403.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oops 403 - Forbidden</title>
+</head>
+<body>
+    
+    <h1>You don't have permission to access this resource.</h1>
+
+</body>
+</html>
